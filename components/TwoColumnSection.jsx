65c8e2a0c22d98ca24af7ebe6cb8b486f73cce11
AsifOni <asif.oni@astralforminc.com>
2023-08-20 21:33:56 -0400

    Upgraded library

--- components/TwoColumnSection.jsx
+++ components/TwoColumnSection.jsx
@@ -1,18 +1,9 @@
 import { TwoColumnSection as CAASCOntTwoColumnSection } from 'ccgx-caascont';
-import { Button } from './Button';
 
 export const TwoColumnSection = (props) => {
-  const { buttonData = {} } = props.cardData || {};
-  const isButtonExist = Object.keys(buttonData).length > 0;
-
   return (
     <div className="py-10 my-10 relative" data-sb-object-id={props.id}>
       <CAASCOntTwoColumnSection {...props} containerStyle="custom-twoColumnSection" />
-      {isButtonExist && (
-        <div className="flex absolute top-[75%] left-[53%]">
-          <Button {...buttonData} />
-        </div>
-      )}
     </div>
   );
 };
